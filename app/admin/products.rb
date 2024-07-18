9ac2d51f941ee2a550cb901f79eb1cc4da3cbb84
Melo <78803336+mellotario@users.noreply.github.com>
2024-07-18 12:12:49 -0500

    add product

--- app/admin/products.rb
+++ app/admin/products.rb
@@ -2,7 +2,7 @@ ActiveAdmin.register Product do
   permit_params :name, :description, :price, :stock_quantity, :image
 
   form do |f|
-    f.inputs do
+    f.inputs "Product Details" do
       f.input :name
       f.input :description
       f.input :price
@@ -10,5 +10,36 @@ ActiveAdmin.register Product do
       f.input :image, as: :file
     end
     f.actions
+  end
+
+  show do
+    attributes_table do
+      row :name
+      row :description
+      row :price
+      row :stock_quantity
+      row :created_at
+      row :updated_at
+      row :image do |product|
+        if product.image.attached?
+          image_tag url_for(product.image)
+        else
+          content_tag(:span, "No image attached")
+        end
+      end
+    end
+    active_admin_comments
+  end
+
+  index do
+    selectable_column
+    id_column
+    column :name
+    column :description
+    column :price
+    column :stock_quantity
+    column :created_at
+    column :updated_at
+    actions
   end
 end
